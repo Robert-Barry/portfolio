8baf38466d84774fe55dadc5cea26b7e6e3aa6db
Robert Barry <rb4080@gmail.com>
2026-04-17 10:25:30 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+
+    return (
+        <BrowserRouter>
+            <header>
+                <h1>Robert_Barry_portfolio</h1>
+                <nav>
+                    
+                </nav>
+            </header>
+        </BrowserRouter>
+    );
+}
+
+export default App
